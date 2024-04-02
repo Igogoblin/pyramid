73c52366930da0acf4771a1d77fa9756df313cb1
KethyI <kot1256@gmail.com>
2024-04-02 15:26:18 +0300

    added button for header

--- pyramid/src/App.jsx
+++ pyramid/src/App.jsx
@@ -1,9 +1,35 @@
 import "./App.css";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <h2>Test deploy</h2>
+      <p>add dev branch</p>
+      <p>add dev branch</p>
+      <p>add dev branch</p>
+      <p>add dev branch</p>
+      <p>add dev branch</p>
+      <p>add dev branch</p>
+      <p>add dev branch</p>
+      <p>add dev branch</p>
+      <p>add dev branch</p>
+      <p>add dev branch</p>
+      <p>add dev branch</p>
+      <p>add dev branch</p>
+      <p>add dev branch</p>
+      <p>add dev branch</p>
+      <p>add dev branch</p>
+      <p>add dev branch</p>
+      <p>add dev branch</p>
+      <p>add dev branch</p>
+      <p>add dev branch</p>
+      <p>add dev branch</p>
+      <p>add dev branch</p>
+      <p>add dev branch</p>
+      <p>add dev branch</p>
+      <p>add dev branch</p>
       <p>add dev branch</p>
     </div>
   );
---
+++ pyramid/src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className='Header'>
+      <h1> Hi </h1>
+      <button className='btn-hover color-5'>BUTTON</button>
+    </div>
+  );
+}
