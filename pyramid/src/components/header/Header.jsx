9fae3051adec9baddbe1733dbea28463387bbbf5
KethyI <kot1256@gmail.com>
2024-04-03 17:01:36 +0300

    change css to module css

--- pyramid/src/components/header/Header.jsx
+++ pyramid/src/components/header/Header.jsx
@@ -1,10 +1,11 @@
-import "./Header.css";
+import s from "./header.module.css";
 
 export default function Header() {
   return (
-    <div className='Header'>
+    <div className={s.header}>
       <h1> Hi </h1>
-      <button className='btn-hover color-5'>BUTTON</button>
+
+      <button className={`${s.btn_hover} ${s.color_5}`}>BUTTON</button>
     </div>
   );
 }
