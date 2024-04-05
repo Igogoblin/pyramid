7e2f41c075e6a41e970d4d834e71cedfcb99c57d
KethyI <kot1256@gmail.com>
2024-04-05 06:45:10 +0300

    add all buttons and download documentation for icons

--- pyramid/src/components/header/Header.jsx
+++ pyramid/src/components/header/Header.jsx
@@ -1,11 +1,15 @@
 import s from "./header.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Header() {
   return (
     <div className={s.header}>
-      <h1> Hi </h1>
-
-      <button className={`${s.btn_hover} ${s.color_5}`}>BUTTON</button>
+      <h1 className={s.tac_one_regular}>Pyramid</h1>
+      <FontAwesomeIcon icon='fa-solid fa-check-square' />
+      <button className={`${s.btn_hover} ${s.color_5}`}>CANCEL</button>
+      <button className={`${s.btn_hover} ${s.color_5}`}>HINT</button>
+      <button className={`${s.btn_hover} ${s.color_5}`}>NEW GAME</button>
+      <button className={`${s.btn_hover} ${s.color_5}`}>RESTART</button>
     </div>
   );
 }
