834043f22b72b1e2e0b1b9c5b2545d1830a5fe1b
KethyI <kot1256@gmail.com>
2024-04-06 06:38:35 +0300

    style logo and icons to buttons

--- pyramid/src/components/header/Header.jsx
+++ pyramid/src/components/header/Header.jsx
@@ -1,15 +1,54 @@
 import s from "./header.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faRotateLeft,
+  faLightbulb,
+  faPlus,
+  faArrowLeft,
+  faGear,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
   return (
     <div className={s.header}>
       <h1 className={s.tac_one_regular}>Pyramid</h1>
-      <FontAwesomeIcon icon='fa-solid fa-check-square' />
-      <button className={`${s.btn_hover} ${s.color_5}`}>CANCEL</button>
-      <button className={`${s.btn_hover} ${s.color_5}`}>HINT</button>
-      <button className={`${s.btn_hover} ${s.color_5}`}>NEW GAME</button>
-      <button className={`${s.btn_hover} ${s.color_5}`}>RESTART</button>
+
+      <button className={`${s.btn_hover} ${s.color_5}`}>
+        {" "}
+        <FontAwesomeIcon
+          icon={faArrowLeft}
+          style={{ color: "#ffffff" }}
+        />{" "}
+        CANCEL
+      </button>
+      <button className={`${s.btn_hover} ${s.color_5}`}>
+        {" "}
+        <FontAwesomeIcon
+          icon={faLightbulb}
+          style={{ color: "#ffffff" }}
+        />{" "}
+        HINT
+      </button>
+      <button className={`${s.btn_hover} ${s.color_5}`}>
+        <FontAwesomeIcon
+          icon={faPlus}
+          style={{ color: "#fafafa" }}
+        />{" "}
+        NEW GAME
+      </button>
+      <button className={`${s.btn_hover} ${s.color_5}`}>
+        <FontAwesomeIcon
+          icon={faRotateLeft}
+          style={{ color: "#ffffff" }}
+        />{" "}
+        RESTART
+      </button>
+      <button className={`${s.btn_hover} ${s.color_5} ${s.no_display}`}>
+        <FontAwesomeIcon
+          icon={faGear}
+          style={{ color: "#ffffff" }}
+        />
+      </button>
     </div>
   );
 }
