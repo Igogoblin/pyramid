0a79fc7a402d9fcc227ebe5922c59b6f6aa88c6a
Ihar Skavysh <igogoblin@gmail.com>
2024-06-04 09:58:11 +0300

    feat: add new config in vite file

--- pyramid/vite.config.js
+++ pyramid/vite.config.js
@@ -2,13 +2,47 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: "/pyramid/",
+//   plugins: [react()],
+//   build: {
+//     minify: false,
+//     sourcemap: true,
+//     imagePath: "/pyramid/src/assets/",
+//     assetsInlineLimit: 0,
+//   },
+// });
 export default defineConfig({
   base: "/pyramid/",
   plugins: [react()],
   build: {
     minify: false,
     sourcemap: true,
-    imagePath: "/pyramid/src/assets/",
+    rollupOptions: {
+      input: {
+        main: "/path/to/main.js", // Указать  путь к основному JS файлу
+      },
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split(".").pop();
+          if (extType === "css") {
+            return "css/[name]-[hash][extname]";
+          } else if (["png", "jpg", "jpeg", "gif", "svg"].includes(extType)) {
+            return "images/[name]-[hash][extname]";
+          } else {
+            return "assets/[name]-[hash][extname]";
+          }
+        },
+      },
+    },
+    publicDir: "public", // Убедиться, что эта директория существует и содержит наши изображения
+    assetsInclude: [
+      "**/*.png",
+      "**/*.jpg",
+      "**/*.jpeg",
+      "**/*.svg",
+      "**/*.gif",
+    ],
     assetsInlineLimit: 0,
   },
 });
